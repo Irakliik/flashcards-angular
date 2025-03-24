9376629b36f534001eedb7171bf64e8a6602446c
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-03-24 19:37:35 +0400

    add flashcards component and a little bit routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CreateSetComponent } from './create-set/create-set.component';
+import { HomeComponent } from './home/home.component';
+import { FlashcardsComponent } from './flashcards/flashcards.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'create-set',
+    component: CreateSetComponent,
+  },
+
+  {
+    path: 'flashcards',
+    component: FlashcardsComponent,
+  },
+];

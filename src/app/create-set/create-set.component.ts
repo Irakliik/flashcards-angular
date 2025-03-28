cea55173ab94ceda9bd267e7ae6ad5e24f48ea57
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-03-28 18:59:22 +0400

    finished part of create-set component

--- src/app/create-set/create-set.component.ts
+++ src/app/create-set/create-set.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CreatingCardComponent } from './creating-card/creating-card.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-set',
   standalone: true,
-  imports: [CreatingCardComponent],
+  imports: [CreatingCardComponent, FormsModule],
   templateUrl: './create-set.component.html',
   styleUrl: './create-set.component.css',
 })
-export class CreateSetComponent {}
+export class CreateSetComponent {
+  constructor(private scrollContainer: ElementRef) {}
+
+  title!: string;
+  description?: string;
+
+  creatingCards: {
+    id: string;
+    term: string;
+    definition: string;
+  }[] = [
+    {
+      id: 'c1',
+      term: '',
+      definition: '',
+    },
+    {
+      id: 'c2',
+      term: '',
+      definition: '',
+    },
+    {
+      id: 'c3',
+      term: '',
+      definition: '',
+    },
+  ];
+
+  onDelete(id: string) {
+    this.creatingCards = this.creatingCards.filter((el) => el.id !== id);
+  }
+
+  onAddCard() {
+    this.creatingCards.push({
+      id: '' + new Date().getTime(),
+      term: '',
+      definition: '',
+    });
+  }
+
+  onSubmit() {
+    console.log(this.creatingCards);
+  }
+
+  onSwap() {
+    this.creatingCards.forEach((el) => {
+      [el.term, el.definition] = [el.definition, el.term];
+    });
+  }
+}
--- src/app/create-set/creating-card/creating-card.component.ts
+++ src/app/create-set/creating-card/creating-card.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'li[appCreatingCard]',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './creating-card.component.html',
   styleUrl: './creating-card.component.css',
   host: {
     class: 'creating-card',
   },
 })
-export class CreatingCardComponent {}
+export class CreatingCardComponent {
+  @Input({ required: true }) creatingCard!: {
+    id: string;
+    term: string;
+    definition: string;
+  };
+
+  @Input({ required: true }) cardNum!: number;
+
+  delete = output<string>();
+
+  onDeleteCreatingCard() {
+    this.delete.emit(this.creatingCard.id);
+  }
+}

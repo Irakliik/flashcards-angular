f1e5aa4e64e07b223109277bc0c1670e4a551a38
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-04-08 14:20:18 +0400

    added sets menue item component

---
+++ src/app/home/sets-menu-item/sets-menu-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sets-menu-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './sets-menu-item.component.html',
+  styleUrl: './sets-menu-item.component.css'
+})
+export class SetsMenuItemComponent {
+
+}

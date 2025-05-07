14a83367652a7e9db56e4beec8f0dc923c8a9058
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-05-07 15:21:40 +0400

    added template drive form on the edit component

--- src/app/flashcards/board/edit-card/edit-card.component.ts
+++ src/app/flashcards/board/edit-card/edit-card.component.ts
@@ -1,28 +1,53 @@
-import { Component, inject, input, model, output, signal } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  inject,
+  input,
+  model,
+  output,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { Card } from '../../../sets-model';
 import { FlashcardsService } from '../../../flashcards.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-card',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './edit-card.component.html',
   styleUrl: './edit-card.component.css',
 })
 export class EditCardComponent {
+  private form = viewChild.required<NgForm>('form');
   private flashcardsService = inject(FlashcardsService);
   selectedCard = model.required<Card>();
   closeModal = output();
   termFocused = false;
   definitionFocused = false;
 
+  constructor() {
+    afterNextRender(() => {
+      setTimeout(() => {
+        this.form().setValue({
+          newTerm: this.selectedCard().term,
+          newDefinition: this.selectedCard().definition,
+        });
+      }, 1);
+    });
+  }
+
   onCloseBtn() {
     this.closeModal.emit();
   }
 
-  onSubmit(newTerm: string, newDefinition: string, e: Event) {
+  onSubmit(e: Event) {
     // e.preventDefault();
     e.preventDefault();
+
+    const newTerm = this.form().form.value.newTerm;
+    const newDefinition = this.form().form.value.newDefinition;
 
     this.selectedCard.update((oldCard) => {
       return { ...oldCard, term: newTerm, definition: newDefinition };
